79393e34d8f0a2a5abfc8bfc49033d99e07e0cb2
Luis Angelo Espinosa <54087395+Cheesy37@users.noreply.github.com>
2023-10-28 23:04:08 +1100

    homepage working and is randomised woo

--- EntGuild/Models/ProductGenreViewModel.cs
+++ EntGuild/Models/ProductGenreViewModel.cs
@@ -6,7 +6,14 @@ namespace EntGuild.Models
     {
         public List<Product>? Products { get; set; }
         public List<SelectListItem>? Genres { get; set; }
+        public List<SelectListItem>? SubGenres { get; set; }
         public int? ProductGenre { get; set; }
+        public List<Product>? BooksProducts { get; set; }
+        public List<Product>? MoviesProducts { get; set; }
+        public List<Product>? GamesProducts { get; set; }
+
         public string? SearchString { get; set; }
+
+
     }
 }
