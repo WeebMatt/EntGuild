de69561ccd855e35dd757e8da67406d9bb611e20
Luis Angelo Espinosa <54087395+Cheesy37@users.noreply.github.com>
2023-10-29 18:47:10 +1100

    REVERTED

--- EntGuild/Models/Order.cs
+++ EntGuild/Models/Order.cs
@@ -1,22 +1,13 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace EntGuild.Models
+﻿namespace EntGuild.Models
 {
     public class Order
     {
 
         public int OrderID { get; set; }
-        [Required]
         public int customer { get; set; }
         public string? StreetAdddress { get; set; }
         public int PostCode { get; set; }
         public string? Suburb { get; set; }
         public string? State { get; set; }
-        public int OrderStatusId { get; set; }
-        public bool IsDeleted { get; set; } = false;
-
-        public OrderStatus OrderStatus { get; set; }
-        public List<OrderDetail> OrderDetail { get; set; }
-
     }
 }
--- EntGuild/Program.cs
+++ EntGuild/Program.cs
@@ -8,10 +8,7 @@ builder.Services.AddDbContext<EntGuildContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("EntGuildContext") ?? throw new InvalidOperationException("Connection string 'EntGuildContext' not found.")));
 
 // Add services to the container.
-builder.Services.AddMemoryCache();
-builder.Services.AddSession();
 builder.Services.AddControllersWithViews();
-builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
@@ -27,8 +24,6 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
-app.UseSession();
 
 app.UseAuthorization();
 
